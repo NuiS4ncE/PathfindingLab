2dd4286201705af64e512eee52f271cf9fdbc878
NuiS4ncE <nuis4nce@hotmail.com>
2021-01-30 01:19:30 +0200

    second week report and GUI building

---
+++ documentation/reports/secondweek.md
@@ -0,0 +1,12 @@
+# Second week
+
+This week I tried to do some configurations on the project. I started with the GUI instead of going straight to the algorithms. The idea was
+to be able to visualize and understand what kind of data the algorithms will handle. 
+So far the creation of the GUI has been somewhat unsuccessful. 
+I've learned once again that JavaFX is a pain in the ass. 
+Also the configuration with codecov, checkstyle and JavaFX Gradle plugin caused some trouble and had to be debugged for hours. 
+Unfortunately I didn't manage my time well and hadn't completed as much as I wanted to. 
+Testing is non-existent right now and javadoc is missing. 
+<br>
+Next up is to make at least a skeleton of the algorithms. The idea is to use the 
+color values of adjacent pixels and use them as nodes. 
--- src/main/java/PathfindingLab/ui/GUI.java
+++ src/main/java/PathfindingLab/ui/GUI.java
@@ -4,9 +4,19 @@ package PathfindingLab.ui;
 //import javax.swing.text.html.ImageView;
 
 import PathfindingLab.io.IOImg;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -22,8 +32,12 @@ public class GUI {
     private Stage primaryStage;
     private BorderPane borderPane;
     private Scene mainScene;
-    private double posX;
-    private double posY;
+    private double startPosX;
+    private double startPosY;
+    private double endPosX;
+    private double endPosY;
+    private GridPane gridPane;
+    private Group root;
 
     /*
     public void initializeGUI() {
@@ -45,38 +59,95 @@ public class GUI {
 
     public Scene buildScene(String stageTitle) {
         primaryStage.setTitle(stageTitle);
+        gridPane = new GridPane();
         borderPane = new BorderPane();
         StackPane stackPane = new StackPane();
+        root = new Group();
 
         mainScene = new Scene(stackPane, 600, 600, Color.BLACK);
-        Canvas canvas = new Canvas(300, 300);
+        Canvas canvas = new Canvas(600, 600);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
-        graphicsContext.setFill(Color.BLUE);
-        graphicsContext.fillRect(10, 10, 300, 300);
+        //graphicsContext.setFill(Color.BLUE);
+        //graphicsContext.fillRect(10, 10, 400, 400);
 
         ImageView imageView = new ImageView("https://www.movingai.com/benchmarks/dao/arena.png");
         imageView.setFitHeight(400);
         imageView.setFitWidth(400);
+
+        ToggleButton startButton = new ToggleButton("Start");
+        ToggleButton endButton = new ToggleButton("End");
+        Button exitButton = new Button("Exit");
+
+        ToggleButton[] toolsArr = {startButton, endButton};
+
+        ToggleGroup tools = new ToggleGroup();
+
+        for (ToggleButton tool : toolsArr) {
+            tool.setMinWidth(90);
+            tool.setToggleGroup(tools);
+            tool.setCursor(Cursor.HAND);
+        }
+
+        exitButton.setOnMouseClicked(e -> {
+            Platform.exit();
+        });
+        exitButton.setPrefSize(50,40);
+
+
+        //ComboBox<String> comboBox = new ComboBox<>();
+        VBox buttonPane = new VBox(10);
+        buttonPane.setPadding(new Insets(5));
+        buttonPane.setStyle("-fx-background-color: #999");
+        buttonPane.setPrefWidth(100);
+
+        buttonPane.getChildren().addAll(startButton, endButton);
+        //topPane.getChildren().addAll(comboBox);
+        stackPane.setAlignment(startButton, Pos.BOTTOM_CENTER);
+        stackPane.setAlignment(endButton, Pos.BOTTOM_RIGHT);
+        stackPane.setAlignment(exitButton, Pos.TOP_RIGHT);
+        stackPane.getChildren().addAll(canvas, imageView, startButton, endButton, exitButton);
+
+
         imageView.setOnMouseClicked(e -> {
-            this.posX = e.getSceneX();
-            this.posY = e.getSceneY();
-            System.out.println("X: " + posX + " Y: " + posY);
-            //Stage popup = new Stage();
-            //popup.initOwner(primaryStage);
-            //popup.show();
+            if(startButton.isSelected()) {
+                this.startPosX = e.getSceneX();
+                this.startPosY = e.getSceneY();
+                System.out.println("X: " + startPosX + " Y: " + startPosY);
+
+                graphicsContext.setFill(Color.RED);
+                graphicsContext.fillOval(this.startPosX, this.startPosY, 20,20);
+            } else if (endButton.isSelected()) {
+                this.endPosX = e.getSceneX();
+                this.endPosY = e.getSceneY();
+                System.out.println("X: " + endPosX + " Y: " + endPosY);
+
+                graphicsContext.setFill(Color.BLUE);
+                graphicsContext.fillOval(this.endPosX, this.endPosY, 20,20);
+
+            }
         });
 
-        stackPane.getChildren().addAll(canvas, imageView);
+        //borderPane.setTop(comboBox);
+        //borderPane.setCenter(stackPane);
+        //borderPane.setBottom(buttonPane);
 
         return mainScene;
     }
 
-    public double getPosX() {
-        return this.posX;
+    public double getStartPosX() {
+        return this.startPosX;
     }
 
     public double getPosY() {
-        return this.posY;
+        return this.startPosY;
+    }
+
+    public double getEndPosX() {
+        return this.endPosX;
+    }
+
+    public double getEndPosY() {
+        return this.endPosY;
     }
 }
